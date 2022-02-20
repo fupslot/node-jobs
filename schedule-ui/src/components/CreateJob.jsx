1de5ca2c3a3288b2e770ab65e8eae5a0faf9ee0a
Eugene Brodsky <fupslot@gmail.com>
2022-02-20 18:07:31 +0200

    styles

--- schedule-ui/src/components/CreateJob.jsx
+++ schedule-ui/src/components/CreateJob.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import humanizeDuration from "humanize-duration";
-import { useJobTypes, useHttp } from "../hooks/useHttp";
+import { useJobTypes } from "../hooks/useHttp";
 import api from "../utils/api";
 
 const CroneRule = ({ jobRule, setJobRule }) => {
@@ -125,20 +125,20 @@ const ScheduledJobs = () => {
                 <div>{type}</div>
                 <div>{rule}</div>
                 <button
-                  className="text-red-300 hover:text-red-500"
+                  className="text-red-300 hover:text-red-500 bg-slate-50 rounded-md px-2 py-2 hover:bg-white"
                   onClick={handleJobRemove(jobId)}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-6 w-6"
+                    className="h-4 w-4"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
                       d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                     />
                   </svg>
@@ -174,7 +174,9 @@ const CreateJob = () => {
 
     api
       .createJob({ jobType, jobRule })
-      .then((job) => console.log(job))
+      .then((job) => {
+        setJobType("none");
+      })
       .catch((error) => setNewJobError(error));
   };
 
